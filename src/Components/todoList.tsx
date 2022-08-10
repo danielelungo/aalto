b6c5c6e910ec095d338d248b4d12c6cffc8d0047
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-10 19:43:36 +0200

    functional pagination

--- src/Components/todoList.tsx
+++ src/Components/todoList.tsx
@@ -1,14 +1,24 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import styled from "styled-components";
 import { Title } from "../Css/commonStyledComponents";
 import { TodoData } from "../types";
 import { AiOutlineClose, AiOutlineCheck } from "react-icons/ai";
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+import ReactPaginate from "react-paginate";
+import "../App.css";
 
 interface TodoListProps {
   filteredData: TodoData[];
 }
 
+const ITEMS_PER_PAGE = 5;
+
 const TodoList: FunctionComponent<TodoListProps> = ({ filteredData }) => {
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const offset = currentPage * ITEMS_PER_PAGE;
+  const pageCount = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
+
   return (
     <Container>
       <TitleContainer>
@@ -18,10 +28,27 @@ const TodoList: FunctionComponent<TodoListProps> = ({ filteredData }) => {
         <Title flexNumber={4}>TITLE</Title>
         <Title flexNumber={1}>COMPLETED</Title>
       </TitleContainer>
-      {filteredData.map((todo) => (
+      <ReactPaginate
+        nextLabel={<FaChevronCircleRight size={"20px"} />}
+        breakLabel="..."
+        previousLabel={<FaChevronCircleLeft size={"20px"} />}
+        pageCount={pageCount}
+        onPageChange={({ selected: selectedPage }) =>
+          setCurrentPage(selectedPage)
+        }
+        renderOnZeroPageCount={undefined}
+        containerClassName="pagination"
+        pageLinkClassName="page-num"
+        breakClassName="page-num"
+        previousClassName="page-num"
+        nextClassName="page-num"
+        activeLinkClassName="active"
+        disabledClassName="pagination-disabled"
+      />
+      {filteredData.slice(offset, offset + ITEMS_PER_PAGE).map((todo) => (
         <Todo key={todo.id}>
           <UserId>{todo.userId}</UserId>
-          <Text>{`${todo.title}   `} </Text>
+          <Text>{todo.title} </Text>
           <Completed>
             {todo.completed ? (
               <AiOutlineCheck color="#60C3EB" size={"30px"} />
