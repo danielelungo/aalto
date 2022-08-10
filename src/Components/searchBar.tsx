96aaa19f9bdc3953b1980c8a6eee894b43fa61ea
Daniele Lungo <daniele.lungo@epicura.it>
2022-08-11 00:21:56 +0200

    fix searchBar css

--- src/Components/searchBar.tsx
+++ src/Components/searchBar.tsx
@@ -4,6 +4,8 @@ import {
   FunctionComponent,
   SetStateAction,
 } from "react";
+import { FaSistrix } from "react-icons/fa";
+import styled from "styled-components";
 
 interface SearchBarProps {
   inputValue: string;
@@ -17,18 +19,37 @@ const SearchBar: FunctionComponent<SearchBarProps> = ({
   onSearch,
 }) => {
   return (
-    <div>
+    <Container>
+      <Button>
+        <FaSistrix onClick={onSearch as any} color="white" size="35px" />
+      </Button>
       <form onSubmit={onSearch}>
-        <input
+        <Input
           type="text"
-          placeholder="search"
+          placeholder="Search..."
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button disabled={inputValue === ""}>search</button>
       </form>
-    </div>
+    </Container>
   );
 };
 
 export default SearchBar;
+
+const Button = styled.div`
+  background-color: #644c79;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+const Input = styled.input`
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
